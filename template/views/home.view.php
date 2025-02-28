bd13042f773f54af14731816d916534b43a7aef1
Ezra Vos <eevos@st.noorderpoort.nl>
2025-02-28 15:06:03 +0100

    Components toegevoegd

--- template/views/home.view.php
+++ template/views/home.view.php
@@ -1,3 +1,11 @@
-<div>
-    <h1 class="text-3xl text-primary">heya</h1>
-</div>
+<?php
+
+include_once '../template/components/header.view.php';
+
+?>
+<body>
+    <div>
+        <h1 class="text-3xl text-primary">heya</h1>
+    </div>
+</body>
+<?php include_once '../template/component/footer.view.php'; ?>
