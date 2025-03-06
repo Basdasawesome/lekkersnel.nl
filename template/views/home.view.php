baf539078093b56c4cae627671f733db263039c3
Cristian_de_Vries <cdevries33@st.noorderpoort.nl>
2025-03-06 11:52:27 +0100

    added hero

--- template/views/home.view.php
+++ template/views/home.view.php
@@ -5,7 +5,8 @@ include_once '../template/components/header.view.php';
 ?>
 <body>
     <div>
-        <h1 class="text-3xl text-primary">heya</h1>
+        <?php include_once '../template/components/hero.php'; ?>
     </div>
 </body>
+</html>
 <?php include_once '../template/components/footer.view.php'; ?>
