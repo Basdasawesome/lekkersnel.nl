c4a94f8fa78fd9821b0d06daf10267fb925a268c
Cristian_de_Vries <cdevries33@st.noorderpoort.nl>
2025-03-06 11:56:30 +0100

    Edit files

--- template/components/hero.php
+++ template/components/hero.php
@@ -2,7 +2,3 @@
 <h1 class="absolute top-56 right-0 left-0 px-4 py-3 mx-auto w-fit text-center drop-shadow-lg rounded-md bg-white             md:px-6 md:py-4 lg:px-8 lg:w-fit">
     ZOEK NAAR EEN LEKKER SNEL RECEPT
 </h1>
-
-
-
-<!-- mx-11 md:mx-24 lg:mx-96 -->
