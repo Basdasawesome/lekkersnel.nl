0fe87693505809ee1b7f3add3323c6e281a7ca18
Ezra Vos <eevos@st.noorderpoort.nl>
2025-03-06 11:14:34 +0100

    Component card_small gemaakt

--- template/views/home.view.php
+++ template/views/home.view.php
@@ -6,6 +6,7 @@ include_once '../template/components/header.view.php';
 <body>
     <div>
         <h1 class="text-3xl text-primary">heya</h1>
+        <?php include_once '../template/components/card_small.view.php' ?>
     </div>
 </body>
 <?php include_once '../template/components/footer.view.php'; ?>
