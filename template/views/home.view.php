45d5c86b14cb338f48a9aea951c762e8fe4977d7
Ezra Vos <eevos@st.noorderpoort.nl>
2025-03-07 10:19:40 +0100

    Home view gemaakt

--- template/views/home.view.php
+++ template/views/home.view.php
@@ -1,14 +1,31 @@
 <?php
 
-include_once '../template/components/header.view.php';
+include_once '../template/components/header.php';
 
 ?>
 
 </body>
+<div>
     <?php include_once '../template/components/navbar.php'; ?>
-    <div>
-    <?php include_once '../template/components/hero.php'; ?>
-    <?php include_once '../template/components/card_small.view.php'; ?>
+    <div class="pt-[70px]">
+        <?php include '../template/components/hero.php'; ?>
     </div>
-    <?php include_once '../template/components/footer.php'; ?>
+    <div class="mt-16">
+        <?php include_once '../template/components/card_medium.php'; ?>
+    </div>
+    <div class="mt-16">
+        <?php include_once '../template/components/card_small.view.php'; ?>
+    </div>
+    <div class="mt-16">
+        <?php include '../template/components/hero.php'; ?>
+    </div>
+    <div class="mt-16">
+        <?php include '../template/components/card_small.view.php' ?>
+    </div>
+    <div class="mt-16">
+        <?php include_once '../template/components/footer.php'; ?>
+    </div>
+</div>
 </body>
+
+</html>
