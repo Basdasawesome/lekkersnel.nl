2d69f3d8c617c2d4ee14a1005e81d5d03792b499
Jada Hooper <jhooper@st.noorderpoort.nl>
2025-03-07 10:23:13 +0100

    added medium cards and fixed small cards

---
+++ template/components/card_medium.php
@@ -0,0 +1,23 @@
+<div class="max-w-7xl mx-auto px-4">
+    <div class="w-full my-10">
+        <h1 class="w-full text-2xl flex items-center gap-2">
+            Onze favorieten
+            <span class="flex-1 ml-6 border-2 border-primary"></span>
+        </h1>
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 sm:gap-12 md:gap-16 mt-10">
+        <div class="w-full relative">
+            <img class="w-full object-cover h-64 min-h-[256px] rounded-lg" src="/img/image.png" alt="Pannenkoeken">
+            <h2 class="absolute bottom-4 left-4 text-white bg-primary p-2 rounded-md opacity-90 text-sm sm:text-base">Pannenkoeken</h2>
+        </div>
+        <div class="w-full relative">
+            <img class="w-full object-cover h-64 min-h-[256px] rounded-lg" src="/img/image.png" alt="Pannenkoeken">
+            <h2 class="absolute bottom-4 left-4 text-white bg-primary p-2 rounded-md opacity-90 text-sm sm:text-base">Pannenkoeken</h2>
+        </div>
+        <div class="w-full relative">
+            <img class="w-full object-cover h-64 min-h-[256px] rounded-lg" src="/img/image.png" alt="Pannenkoeken">
+            <h2 class="absolute bottom-4 left-4 text-white bg-primary p-2 rounded-md opacity-90 text-sm sm:text-base">Pannenkoeken</h2>
+        </div>
+    </div>
+</div>
--- template/views/home.view.php
+++ template/views/home.view.php
@@ -8,7 +8,8 @@ include_once '../template/components/header.view.php';
     <?php include_once '../template/components/navbar.php'; ?>
     <div>
     <?php include_once '../template/components/hero.php'; ?>
-    <?php include_once '../template/components/card_small.view.php'; ?>
+    <?php include_once '../template/components/card_small.php'; ?>
+    <?php include_once '../template/components/card_medium.php'; ?>
     </div>
     <?php include_once '../template/components/footer.php'; ?>
 </body>
