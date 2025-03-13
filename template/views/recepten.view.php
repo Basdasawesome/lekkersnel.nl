c6ab3b52713db820e0f32f31e460b3d06ec0c824
Jada Hooper <jhooper@st.noorderpoort.nl>
2025-03-13 11:26:23 +0100

    added recepten page

---
+++ template/views/recepten.view.php
@@ -0,0 +1,6 @@
+<?php include_once '../template/components/header.php'; ?>
+<?php include_once '../template/components/hero.php'; ?>
+<?php include_once '../template/components/card_medium.php'; ?>
+<?php include_once '../template/components/card_small.php'; ?>
+<?php include_once '../template/components/card_medium.php'; ?>
+<?php include_once '../template/components/footer.php'; ?>
