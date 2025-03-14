f73398bfbc9090734c26a49b2cad6860b578ae15
daveymunters87 <115430210+daveymunters87@users.noreply.github.com>
2025-03-14 09:48:55 +0100

    Fix: Fixed navbar size

--- public/index.php
+++ public/index.php
@@ -9,4 +9,4 @@ require_once __DIR__ . '/../src/helpers/auth.php';
 
 
 getPage();
-checkAuth();
+// checkAuth();
--- template/components/header.php
+++ template/components/header.php
@@ -20,7 +20,7 @@ $currentPage = isset($_GET['page']) ? $_GET['page'] : 'home';
 <body>
 
 <nav class="h-[72px] w-full border-b border-gray-200 bg-white z-50">
-    <div class="container mx-auto px-4 flex justify-between items-center py-3">
+    <div class="max-w-7xl mx-auto px-4 flex justify-between items-center py-3">
         <!-- Logo -->
         <a href="?page=home" class="flex items-center">
             <img src="img/logo.png" alt="logo" class="w-12 h-12">
